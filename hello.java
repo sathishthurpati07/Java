19e5aa5020c3798bed66c93def876aa6ff633712
sathishthurpati07 <thurpatisathish07@gmail.com>
2025-12-10 16:14:55 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,79 @@
+/*public class hello{
+     public static void main(String[] args) {
+      float Num1 = 23;
+      float Num2 = 13;
+      float Add = Num1+Num2;
+      float DIV = Num1/Num2;
+      float Sub = Num1-Num2;
+      float Mul = Num1*Num2;
+        System.out.println("ADDition :"+Add);
+        System.out.println("Division :"+DIV);
+        System.out.println("Subtraction :"+Sub);
+        System.out.println("Multiplication :"+Mul);
+     }
+} 
+public class hello {
+     public static void main(String[] args) {
+       int num = 86;
+     if(num%2==0){
+          System.out.println("The Number is EVEN");
+     }else{
+          System.out.println("The Number is ODD");
+     }
+         
+     }
+    
+}
+public class hello {
+
+     public static void main(String[] args) {
+          System.out.println("Mr.Kalyan Dodla");
+          int English = 96;
+          int Science = 76;
+          int maths = 95;
+          int Social = 69;
+          int Java = 85;
+          int Sum = English+Science+maths+Social+Java;
+          int Avg = Sum/5;
+          if(Avg>90){
+               System.out.println("Grade is 'O'");
+          }
+          else if(Avg<90 && Avg>80){
+               System.out.println("Grade is 'A'");
+          }
+          else if(Avg>70){
+               System.out.println("Grade is B");
+          }
+          else if(Avg>60){
+               System.out.println("Grade is 'C'");
+          }
+          else{
+               System.out.println("Grade is D");
+          }
+     }
+
+import java.util.Scanner;
+public class hello {
+   public static void main(String[] args) {
+     Scanner Sc = new Scanner(System.in);
+     System.out.println("Hi{Mr.Sathish}!");
+   }
+} */
+import java.util.Scanner;
+public class hello{
+     public static void main(String[] args) {
+          Scanner Sc = new Scanner(System.in);
+          System.out.println("Enter Value of X:");
+          int X = Sc.nextInt();
+          System.out.println("Enter Value of Y");
+          int Y = Sc.nextInt();
+          int sum = X+Y;
+          int Dif = X-Y;
+          int mul = X*Y;
+          int Div = X/Y;
+          System.out.println("The sum is" +sum);
+          System.out.println("The dif is" +Dif);
+          System.out.println("The mul is" +mul);
+          System.out.println("The div is" +Div);
+     }
+}
